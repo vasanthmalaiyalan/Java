cade8cbd0e862353f7a2c1cbd55ed236b64d5bc0
vasanthmalaiyalan <vasanthmalaiyalan321@gmail.com>
2026-04-14 09:23:03 +0000

    BlockDemo.class,BlockDemo.java,

---
+++ Java A Beginners Guide Create, Compile, and Run Java Programs Today (Herbert Schildt)/BlockDemo.java
@@ -0,0 +1,20 @@
+class BlockDemo {
+    public static void main(String[] args) {
+
+        double i, j, d;
+
+        i = 5;
+        j = 10;
+
+        if(i != 0) {
+            System.out.println("i does not equal zero");
+            d = j / i;
+            System.out.println("j / i is " + d);
+        }
+    } 
+}
+
+/*
+i does not equal zero
+j / i is 2.0
+*/
