efae17aaee555086cb0569623c0902e54e17d4ec
vasanthmalaiyalan <vasanthmalaiyalan321@gmail.com>
2026-04-14 12:45:57 +0000

    CharArithDemo.class,CharArithDemo.java,

---
+++ Java A Beginners Guide Create, Compile, and Run Java Programs Today (Herbert Schildt)/Chapter-2/CharArithDemo.java
@@ -0,0 +1,15 @@
+class CharArithDemo {
+    public static void main(String[] args) {
+
+        char ch;
+
+        ch = 'X';
+        System.out.println("ch contains " + ch);
+
+        ch++; // Increament 'X' → next character → 'Y'
+        System.out.println("ch contains " + ch);
+
+        ch = 90; // ASCII Value 90 = z
+        System.out.println("ch contains " + ch);
+    }
+}
