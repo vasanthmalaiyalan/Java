c2b39673d4fc6216b73d0f570be381452eaee5b2
vasanthmalaiyalan <vasanthmalaiyalan321@gmail.com>
2026-04-14 13:21:19 +0000

    NumberSystem.class,NumberSystem.java,

---
+++ Java A Beginners Guide Create, Compile, and Run Java Programs Today (Herbert Schildt)/Chapter-2/NumberSystem.java
@@ -0,0 +1,14 @@
+class NumberSystem {
+    public static void main(String[] args) {
+
+        int dec = 10;
+        int bin = 0b1100;
+        int oct = 011;
+        int hex = 0xFF;
+
+        System.out.println("Decimal: " + dec);
+        System.out.println("Binary: " + bin);
+        System.out.println("Ocatal: " + oct);
+        System.out.println("Hex: " + hex);
+    }
+}
