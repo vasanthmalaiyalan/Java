195112a76fd07c205705dfbff713fc26b47e10d7
vasanthmalaiyalan <vasanthmalaiyalan321@gmail.com>
2026-04-14 07:58:23 +0000

    Example2.class,Example2.java,Applet 2 Mordern Evolve .md,Bytecode java .md,Encapsulation.md,Inheritance.md,JAVA_HOME set.md,JDK + JRE + JVM.md,JDK Install.md,Java Buzzwords.md,Java evolving.md,Java family tree.md,Java portability.md,LTS versions.md,OOP.md,Polymorphism.md,java internet.md,

---
+++ Java A Beginners Guide Create, Compile, and Run Java Programs Today (Herbert Schildt)/Example2.java
@@ -0,0 +1,16 @@
+class Example2 {
+    public static void main(String[] args) {
+
+        int myVar1;
+        int myVar2;
+
+        myVar1 = 1024; // variable assign value
+
+        System.out.println("myVar1 contains " + myVar1);
+
+        myVar2 = myVar1 / 2;
+
+        System.out.print("myVar2 contains myVar1 / 2: ");
+        System.out.println(myVar2);
+    } 
+}
