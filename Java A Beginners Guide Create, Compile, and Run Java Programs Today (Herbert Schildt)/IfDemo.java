3d4702b8dede77286a49c540bca06b782cc8d9ee
vasanthmalaiyalan <vasanthmalaiyalan321@gmail.com>
2026-04-14 08:35:40 +0000

    IfDemo.java,

---
+++ Java A Beginners Guide Create, Compile, and Run Java Programs Today (Herbert Schildt)/IfDemo.java
@@ -0,0 +1,31 @@
+class IfDemo {
+    public static void main(String[]  args) {
+
+        int a, b, c;
+
+        a = 2;
+        b = 3;
+
+        if(a < b) System.out.println("a is less than b");
+
+        if(a == b) System.out.println("you won't see this");
+
+        System.out.println() //just black space
+
+        c = a - b; // -1
+
+        System.out.println("C contains -1");
+
+        if(c >= 0) System.out.println("c is no-negative number");
+        if(c < 0) System.out.println("c is negative");
+
+        System.out.println();
+
+        c = b - 1;
+
+        System.out.println("c contains 1");
+
+        if(c >= 0) System.out.println("c is non-negative");
+        if(c < 0) System.out.println("c is negative");
+    }
+}
