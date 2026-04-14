98ddf6ac9a052bb23d7720497d53ea2bc8d06faa
vasanthmalaiyalan <vasanthmalaiyalan321@gmail.com>
2026-04-14 09:06:18 +0000

    ForDemo.class,ForDemo.java,

---
+++ Java A Beginners Guide Create, Compile, and Run Java Programs Today (Herbert Schildt)/ForDemo.java
@@ -0,0 +1,20 @@
+class ForDemo {
+    public static void main(String[] args) {
+
+        int count;
+
+        for(count = 0; count < 5; count = count + 1)
+            System.out.println("This is count: " + count);
+
+        System.out.println("Done!");
+    }
+}
+
+/*
+This is count: 0
+This is count: 1
+This is count: 2
+This is count: 3
+This is count: 4
+Done!
+*/
