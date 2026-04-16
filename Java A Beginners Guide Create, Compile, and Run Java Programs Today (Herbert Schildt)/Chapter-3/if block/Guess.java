f60897622ca56967fb64ac93d19dbae5a5497731
vasanthmalaiyalan <vasanthmalaiyalan321@gmail.com>
2026-04-16 06:22:39 +0000

    Guess.class,Guess.java,

---
+++ Java A Beginners Guide Create, Compile, and Run Java Programs Today (Herbert Schildt)/Chapter-3/if block/Guess.java
@@ -0,0 +1,15 @@
+class Guess {
+    public static void main(String[] args)
+      throws java.io.IOException {
+
+        char ch, answer = 'k';
+
+        System.out.println("I'm thinking of a letter between A and Z. ");
+        System.out.print("Can you guess it: ");
+
+        ch = (char) System.in.read();
+
+        if (ch == answer)
+          System.out.println("** Right **");
+      }
+}
