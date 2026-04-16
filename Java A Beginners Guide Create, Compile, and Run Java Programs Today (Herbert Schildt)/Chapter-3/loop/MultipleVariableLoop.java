23120824e58f537f7f2f75c124d92ac69a17de02
vasanthmalaiyalan <vasanthmalaiyalan321@gmail.com>
2026-04-16 11:23:15 +0000

    Comma.class,InputLoop.class,InputLoop.java,MultipleVariableLoop.class,MultipleVariableLoop.java,

---
+++ Java A Beginners Guide Create, Compile, and Run Java Programs Today (Herbert Schildt)/Chapter-3/loop/MultipleVariableLoop.java
@@ -0,0 +1,8 @@
+class MultipleVariableLoop {
+    public static void main(String[] args) {
+        int i, j;
+
+        for(i = 0, j = 10; i < j; i++, j--)
+          System.out.println("i and j: " + i + " " + j);
+    }
+}
