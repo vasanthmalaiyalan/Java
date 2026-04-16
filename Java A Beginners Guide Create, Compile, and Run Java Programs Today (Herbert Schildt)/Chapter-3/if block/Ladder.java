166f979f16d34a938069065531af312a9a448b53
vasanthmalaiyalan <vasanthmalaiyalan321@gmail.com>
2026-04-16 07:09:51 +0000

    Ladder.class,Ladder.java,

---
+++ Java A Beginners Guide Create, Compile, and Run Java Programs Today (Herbert Schildt)/Chapter-3/if block/Ladder.java
@@ -0,0 +1,23 @@
+class Ladder {
+    public static void main(String[] args){
+        int x;
+
+        for (x = 0; x < 6; x++) {
+
+            if (x == 1)
+              System.out.println("X is one");
+
+            else if (x == 2)
+              System.out.println("X is two");
+
+            else if (x == 3)
+               System.out.println("X is three");
+
+            else if (x == 4)
+               System.out.println("X is four");
+
+            else 
+               System.out.println("x is not between 1 and 4");          
+        }
+}
+}
