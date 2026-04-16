464adacef8ce512019d570c4005d2976e1d2e9db
vasanthmalaiyalan <vasanthmalaiyalan321@gmail.com>
2026-04-16 11:45:26 +0000

    InfinateLoop.class,InfinateLoop.java,LoopWithoutBody.class,LoopWithoutBody.java,

---
+++ Java A Beginners Guide Create, Compile, and Run Java Programs Today (Herbert Schildt)/Chapter-3/loop/InfinateLoop.java
@@ -0,0 +1,8 @@
+class InfinateLoop {
+    public static void main(String[] args) {
+
+        for(;;) {
+            System.out.println("Running....");
+        }
+    }
+}
---
+++ Java A Beginners Guide Create, Compile, and Run Java Programs Today (Herbert Schildt)/Chapter-3/loop/LoopWithoutBody.java
@@ -0,0 +1,10 @@
+class LoopWithoutBody {
+    public static void main(String[] args) {
+        int i;
+        int sum = 0;
+
+        for(i = 1; i <= 5; sum += i++) ;
+
+        System.out.println("Sum is " + sum);
+    }
+}
