ed1292cde74ef907f9f7f581c3f194ed15763f8b
vasanthmalaiyalan <vasanthmalaiyalan321@gmail.com>
2026-04-16 12:30:54 +0000

    LoopBreak.class,LoopBreak.java,

---
+++ Java A Beginners Guide Create, Compile, and Run Java Programs Today (Herbert Schildt)/Chapter-3/loop/LoopBreak.java
@@ -0,0 +1,12 @@
+class LoopBreak {
+    public static void main(String[] args) {
+        int num = 100;
+
+        for (int i = 0; i < num; i++) {
+            if (i * i >= num) break;
+            System.out.print(i + " ");
+        }
+
+        System.out.println("Loop Complete. ");
+    }
+}
