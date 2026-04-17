0f9ea76ef102a113925571e7d4e05fed74ecc962
vasanthmalaiyalan <vasanthmalaiyalan321@gmail.com>
2026-04-17 12:31:09 +0000

    Vehicle.class,VehicleDemo.class,VehicleDemo.java,

---
+++ Java A Beginners Guide Create, Compile, and Run Java Programs Today (Herbert Schildt)/Chapter-4/VehicleDemo.java
@@ -0,0 +1,24 @@
+// program that uses the Vehicle class
+class Vehicle {
+    int passengers; // number of passengers
+    int fuelcap; // fuel capacity in gallons
+    int mpg; // fuel consumption in miles per gallon
+}
+
+// This class declares an object of type Vehicle.
+class VehicleDemo {
+    public static void main(String[] args) {
+        Vehicle minivan = new Vehicle();
+        int range;
+
+        // Assign values to fields in minivan
+        minivan.passengers = 7;
+        minivan.fuelcap = 16;
+        minivan.mpg = 21;
+
+        // compute the range assuming a full tank of gas
+        range = minivan.fuelcap * minivan.mpg;
+
+        System.out.println("Minivan can carry " + minivan.passengers + " with a range of " + range);
+    }
+}
