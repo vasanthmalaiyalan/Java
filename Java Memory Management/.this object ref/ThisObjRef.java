adf02c47fc703bd80bcc30b372e7cbd8630cf043
vasanthmalaiyalan <vasanthmalaiyalan321@gmail.com>
2026-04-18 11:55:23 +0000

    Person.class,ThisObjRef.class,ThisObjRef.java,

---
+++ Java Memory Management/.this object ref/ThisObjRef.java
@@ -0,0 +1,19 @@
+class Person {
+    int age;
+
+    void SetAge(int age) {
+        this.age = age;
+    }
+
+    void display() {
+        System.out.println("Age: " + age);
+    }
+}
+
+public class ThisObjRef{
+     public static void main(String[] args) {
+        Person p = new Person(); // object creation and this is index 0
+        p.SetAge(25); // method call
+        p.display();  // print value
+}
+}
