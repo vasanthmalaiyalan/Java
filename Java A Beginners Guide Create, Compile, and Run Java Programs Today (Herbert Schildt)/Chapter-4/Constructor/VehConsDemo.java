c0115860f85e7aa56bddb2637cfd5c14603e3f57
vasanthmalaiyalan <vasanthmalaiyalan321@gmail.com>
2026-04-19 16:26:27 +0000

    VehConsDemo.class,VehConsDemo.java,Vehicle.class,

--- Java A Beginners Guide Create, Compile, and Run Java Programs Today (Herbert Schildt)/Chapter-4/Constructor/VehConsDemo.java
+++ Java A Beginners Guide Create, Compile, and Run Java Programs Today (Herbert Schildt)/Chapter-4/Constructor/VehConsDemo.java
@@ -3,13 +3,34 @@ class Vehicle {
     int fuelcap;
     int mpg;
 
-    Vehical(int p, int f, int m) {
+    Vehicle(int p, int f, int m) {
         passengers = p;
         fuelcap = f;
         mpg = m;
     }
 
     int range() {
-        
+        return mpg * fuelcap;
+    }
+
+    double fuelNeeded(int miles) {
+        return (double) miles / mpg;
+    }
+}
+
+class VehConsDemo {
+    public static void main(String[] args) {
+
+        Vehicle minivan = new Vehicle(7, 16, 21);
+        Vehicle sportscar = new Vehicle(2, 14, 22);
+
+        double gallons;
+        int dist = 252;
+
+        gallons = minivan.fuelNeeded(dist);
+        System.out.println("To go " + dist + " miles minivan needs " + gallons + " gallons of fuel.");
+
+        gallons = sportscar.fuelNeeded(dist);
+        System.out.println("To go " + dist + " miles sportscar needs " + gallons + " gallons of fuel.");
     }
 }
