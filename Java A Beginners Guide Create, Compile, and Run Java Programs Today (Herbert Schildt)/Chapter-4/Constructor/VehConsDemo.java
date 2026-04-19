db8f7dfb93efaf60c64d31404f4483ad39e1de84
vasanthmalaiyalan <vasanthmalaiyalan321@gmail.com>
2026-04-19 16:18:48 +0000

    VehConsDemo.java,

---
+++ Java A Beginners Guide Create, Compile, and Run Java Programs Today (Herbert Schildt)/Chapter-4/Constructor/VehConsDemo.java
@@ -0,0 +1,15 @@
+class Vehicle {
+    int passengers;
+    int fuelcap;
+    int mpg;
+
+    Vehical(int p, int f, int m) {
+        passengers = p;
+        fuelcap = f;
+        mpg = m;
+    }
+
+    int range() {
+        
+    }
+}
