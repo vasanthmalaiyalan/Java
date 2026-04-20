1e2f02c7904b00f0785ee44f1c7ef764570ad0c0
vasanthmalaiyalan <vasanthmalaiyalan321@gmail.com>
2026-04-20 05:04:25 +0000

    PrimitiveVsObjectRef.class,PrimitiveVsObjectRef.java,person.class,

---
+++ Java Memory Management/Heap/Object inside/PrimitiveVsObjectRef.java
@@ -0,0 +1,24 @@
+class person {
+    int age; // primitive
+    String name; // reference (object)
+
+    void display() {
+        System.out.println("Age: " + age);
+        System.out.println("Name: " + name);
+    }
+}
+
+public class PrimitiveVsObjectRef {
+    public static void main(String[] args) {
+
+        // object creation
+        person p = new person();
+
+        // Assign values
+        p.age = 25; // primitive (direct value)
+        p.name = "vasanth"; //reference (point to string pool)
+
+        // Display
+        p.display();
+    }
+}
