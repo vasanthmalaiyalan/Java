777c6f784fdffc8a211524ed7d382781f14ebd70
vasanthmalaiyalan <vasanthmalaiyalan321@gmail.com>
2026-04-20 15:40:49 +0000

    PolymorPhism.java,

--- Java Memory Management/Chaper-2/Abstract vs interface vs extends vs implements/reference vs object + abstract/PolymorPhism.java
+++ Java Memory Management/Chaper-2/Abstract vs interface vs extends vs implements/reference vs object + abstract/PolymorPhism.java
@@ -8,5 +8,38 @@ abstract class Human { // like parent enforce
 
 class Person extends Human implements Walkable {
 
-    
+    @Override
+    void work() {
+        System.out.println("Person working in office");
+    }
+
+    @Override
+    public void walk() {
+        System.out.println("Person walking");
+    }
+}
+
+class Robot extends Human {
+
+    @Override
+    void work() {
+        System.out.println("Robot working automatically");
+    }
+}
+
+public class PolymorPhism {
+
+    public static void main(String[] args) {
+
+        // Polymorphism
+        Human h1 = new Person();
+        Human h2 = new Robot();
+
+        h1.work(); // Person work
+        h2.work(); // Robot work
+
+        // Interface polymorphism
+        Walkable w = new Person();
+        w.walk();
+    }
 }
