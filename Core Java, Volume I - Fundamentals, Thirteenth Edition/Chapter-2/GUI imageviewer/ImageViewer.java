e40bee66d01ee2096fdc3070d33a1ee3ca7aced5
vasanthmalaiyalan <vasanthmalaiyalan321@gmail.com>
2026-04-23 16:54:18 +0000

    ImageViewer.class,ImageViewer.java,ImageViewerFrame.class,image.png,

--- Core Java, Volume I - Fundamentals, Thirteenth Edition/Chapter-2/GUI imageviewer/ImageViewer.java
+++ Core Java, Volume I - Fundamentals, Thirteenth Edition/Chapter-2/GUI imageviewer/ImageViewer.java
@@ -1,101 +1,138 @@
 // GUI related Classes (Windows, Components, events)
-import java.awt.*;
+// ============ IMPORTS (Library package) =========
+// java.awt package -> GUI event handling classes
+import java.awt.*;  // (library)
 
 // File handling (File class)
-import java.io.*;
+// java.io. package -> File handling
+import java.io.*; // (library)
 
 // Swing GUI library (JFrame, JLabel, Jmenu, etc. )
-import javax.swing.*;
+// Swing GUI Components
+import javax.swing.*;   // (library)
 
+//======== USER-DEFINED CLASS =========
 public class ImageViewer {
     public static void main(String[] args) {
+
         // runGUI Code safe UI Thread
-        EventQueue.invokeLater(() -> {
+        // EvenQueue (library class) + invokeLater() (library method)
+        EventQueue.invokeLater(() -> { // (class + method)
 
             // Custom window object create
-            var frame = new ImageViewerFrame();
+            // ImageViewerFrame() -> constructor (user-defined class)
+            var frame = new ImageViewerFrame(); // (constructor call)
 
             // windows title set
-            frame.setTitle("ImageViewer");
+            // setTitle() -> library method (JFrame class )
+            frame.setTitle("ImageViewer"); // (method)
 
             // Close Button Click
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            // setDefaultCloseOperation() -> library method
+            // JFrame.EXIT_ON_CLOSE -> library constant
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //(method + constant)
 
             // windows display view
-            frame.setVisible(true);
+            // setVisible() -> library method
+            frame.setVisible(true); // (method)
         });
     }
 }
 
 // JFrame (windows) Connect extend Custom UI
-class ImageViewerFrame extends JFrame {
-    
+// ===== User-Defined CLASS (inherits library  class)
+class ImageViewerFrame extends JFrame { // JFrame -> (library class)
+
     // windows width (constant)
+    // USER Constants
     private static final int DEFAULT_WIDTH = 300;
 
     // windows height (constant)
     private static final int DEFAULT_HEIGHT = 400;
 
     // Constructor -> object create run
+    // ====== CONSTRUCTOR (user-defined) ========
     public ImageViewerFrame() {
-
+        
         //windows size set
-        setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+        // setSize() -> library method (JFrame)
+        setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT); // (method)
 
         // Image Show need Label create
-        var label = new JLabel();
+        // JLabel() -> library constructor
+        var label = new JLabel(); // (class -> constructor)
 
-        // label add to windows 
-        add(label);
+        // label add to windows
+        // add() -> library method (Constainer class via JFrame) 
+        add(label); // (method)
 
         // file chooser dialog create (file select)
-        var chooser = new JFileChooser();
+        // JFileChooser() -> library constructor
+        var chooser = new JFileChooser();  // (class + constructor)
 
         // default folder -> current director
-        chooser.setCurrentDirectory(new File("."));
+        // File() -> library constructor
+        chooser.setCurrentDirectory(new File(".")); // (method + constructor)
 
         // menu bar create (top bar)
-        var menuBar = new JMenuBar();
+        // JMenuBar() -> library constructor
+        var menuBar = new JMenuBar();  // (class + constructor)
 
         // windows menu bar attach
-        setJMenuBar(menuBar);
+        // setJMenuBar() -> library method
+        setJMenuBar(menuBar);   // (method)
 
         // "File" menu create
-        var menu = new Jmenu("File");
+        // JMenu() -> library constructor
+        var menu = new JMenu("File");   // (class + constructor)
 
         // menu bar add
-        menuBar.add(menu);
+        // add() -> library method
+        menuBar.add(menu);   // (method)
 
         // "Open" menu item create
-        var openItem = new JMenuItem("Open");
+        // JMenuItem() -> library constructor
+        var openItem = new JMenuItem("Open"); // (class + constructor)
 
         // File menu add
-        menu.add(openItem);
+        // add() -> library method
+        menu.add(openItem);  // (method)
 
         // Open click event handle
-        openItem.addActionListener(event -> 
+        // addActionListener() -> library method
+        openItem.addActionListener(event ->  // (method + lambda)
         {
             // file chooser open
-            int result = chooser.ShowOpenDialog(null);
+            // showOpenDialog() -> library method
+            int result = chooser.showOpenDialog(null);  // (method)
 
             // user file select 
+            // JFileChooser.APPROVE_OPTION -> library constant
             if (result == JFileChooser.APPROVE_OPTION)
             {
                 // Selected file path
-                String name = chooser.getSelectedFile().getPath();
+                // getSelectedFile() -> library method
+                // getPath() ->library method
+                String name = chooser.getSelectedFile().getPath(); // (method chain)
 
                 // image load label
-                label.setIcon(new ImageIcon(name));
+                // ImageIcon() -> library constructor
+                // setIcon() -> library method
+                label.setIcon(new ImageIcon("name")); // (constructor + method)
             }
         });
 
         // "Exit" menu item create
-        var exitItem = new JMenuItem("Exit");
+        // JMenuItem() -> library constructor
+        var exitItem = new JMenuItem("Exit");  // (class + constructor)
 
         // menu add
-        menu.add(exitItem);
+        // add() -> library method
+        menu.add(exitItem);   // (method)
 
         // Exit Click -> program close
-        exitItem.addActionListener(event -> System.exit(0));
+        // addActionListener() -> library method
+        // System.exit() -> library method
+        exitItem.addActionListener(event -> System.exit(0)); // (method)
     }
 } 
